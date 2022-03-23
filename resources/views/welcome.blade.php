d3d2193a5ff6e485a0e459daa5166b0e3c7a555a
Juanchis <juanchismo10@gmail.com>
2022-03-22 21:20:25 -0300

    Vista de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,53 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Tailwind Starter Template - Responsive Header : Tailwind Toolbox</title>
+      <meta name="author" content="name">
+      <meta name="description" content="description here">
+      <meta name="keywords" content="keywords,here">
+      <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+      <!--Replace with your tailwind.css once created-->
+      <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+      <!--Totally optional :) -->
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
-        <title>Laravel</title>
+      @livewireStyles
+   </head>
 
-        @livewireStyles
-    </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            
-            <h1>Vista welcome</h1>
-        </div>
+   <body class="bg-gray-100 font-sans leading-normal tracking-normal">
+      <div class="flex md:flex-row-reverse flex-wrap">
 
-        @livewireScripts
-    </body>
+		 <!--Main Content-->
+         <div class="w-full md:w-4/5 bg-gray-100 ">
+            <div class="container bg-gray-100 pt-16 px-6">
+                <h1 class="font-medium leading-tight text-5xl mt-0 mb-2 text-pink-500 text-center">Bienvenidos...</h1>
+            </div>
+         </div>
+
+		 <!--Sidebar-->
+         <div class="w-full md:w-1/5 bg-gray-900 md:bg-gray-900 px-2 text-center fixed bottom-0 md:pt-8 md:top-0 md:left-0 h-16 md:h-screen md:border-r-4 md:border-gray-600">
+            <div class="md:relative mx-auto lg:float-right lg:px-6">
+               <ul class="list-reset flex flex-row md:flex-col text-center md:text-left">
+                  <li class="mr-3 flex-1">
+                     <a href="{{route('login')}}" class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-pink-500">
+
+                     <i class="fas fa-link pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Iniciar sesión</span>
+                     </a>
+                  </li>
+                  <li class="mr-3 flex-1">
+                     <a href="{{route('register')}}" class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-pink-500">
+                     <i class="fas fa-link pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Registrarse</span>
+                     </a>
+                  </li>
+
+               </ul>
+            </div>
+         </div>
+      </div>
+
+      @livewireScripts
+   </body>
 </html>
