62b94be152f4a8cf84460f6470278dad2e0cce18
Juanchis <juanchismo10@gmail.com>
2022-03-25 00:05:35 -0300

    Maquetación de los posts

--- resources/views/livewire/post-component.blade.php
+++ resources/views/livewire/post-component.blade.php
@@ -1,8 +1,32 @@
 <div>
     @forelse ($posts as $post)
-        {{ $post->content }}
+        <!--Title-->
+        <div class="font-sans pb-3">
+            <a href="#" class="text-base md:text-sm text-green-500 font-bold no-underline hover:underline">{{ $post->user->name }}</a>
+            <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">Welcome to Minimal Blog</h1>
+            <p>
+                {{ $post->content }}
+            </p>
+            <div class="flex justify-between">
+
+                {{-- like --}}
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                </a>
+
+                {{-- date --}}
+                <p class="text-sm text-right font-normal text-gray-400">Published {{ $post->created_at->diffForHumans() }}
+                </p>
+            </div>
+        </div>
+
+        <hr>
         <br>
     @empty
         <h2>No hay posts para ver...</h2>
     @endforelse
 </div>
+
+
