ccd9bf155676cc047707b0379b395d80a7285cd4
Juanchis <juanchismo10@gmail.com>
2022-03-29 19:40:40 -0300

    Corrijo error al mostrar los mensajes de error

--- resources/views/livewire/form-component.blade.php
+++ resources/views/livewire/form-component.blade.php
@@ -20,15 +20,7 @@
         </div>
 
         {{-- error message --}}
-        @error('post')
-            <div class="-m-2 text-center">
-                <div class="p-2">
-                <div class="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
-                    <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-3 justify-center items-center">Pink</span>
-                    <span class="inline-flex px-2">{{ $message }}</span>
-                </div>
-            </div>
-        @enderror
+        @include('partials.error')
         {{-- ----------------------------------------------------- --}}
     </div>
 </div>
--- resources/views/partials/error.blade.php
+++ resources/views/partials/error.blade.php
@@ -6,7 +6,7 @@
                 style="border:1px solid #ec4899; display:flex;justify-content:center;"
                 class="w-full inline-flex shadow items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
                 <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-3 justify-center items-center" style="background-color:#ec4899;">Ups!</span>
-                <span class="inline-flex px-2" style="color:#ec4899;">{{ $message }}</span>
+                <span class="inline-flex px-2" style="color:#ec4899;">{{ $message }} </span>
             </div>
         </div>
     @enderror
