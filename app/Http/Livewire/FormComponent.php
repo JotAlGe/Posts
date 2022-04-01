9d8d6cf0cb2f890095058e69dfb8661baaca1a69
Juanchis <juanchismo10@gmail.com>
2022-03-31 23:31:39 -0300

    Creación del input de imagen en el posteo y validación

--- app/Http/Livewire/FormComponent.php
+++ app/Http/Livewire/FormComponent.php
@@ -5,18 +5,26 @@ namespace App\Http\Livewire;
 use App\Models\Post;
 use GrahamCampbell\ResultType\Success;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class FormComponent extends Component
 {
+
+    use WithFileUploads;
+
+    public $photo;
     public $post;
     protected $rules = [
-        'post' => ['required', 'min:5', 'max:255']
+        'post' => ['required', 'min:5', 'max:255'],
+        'photo' => ['mimes:jpg, png', 'max:2048']
     ];
 
     protected $messages = [
         'post.required' => 'Debes escribir algo...',
         'post.min' => 'Postea algo con más caracteres...',
-        'post.max' => 'El post debe contener menos de 255 caracteres'
+        'post.max' => 'El post debe contener menos de 255 caracteres',
+        'photo.max' => 'La imagen es demasiado grande',
+        'photo.mimes' => 'La imagen debe ser png o jpg'
     ];
 
     public function updated($propertyName)
---
+++ resources/views/partials/error-image.blade.php
@@ -0,0 +1,13 @@
+{{-- error message --}}
+    @error('photo')
+        <div class="-m-2 text-center">
+            <div class="p-2">
+            <div
+                style="border:1px solid #ec4899; display:flex;justify-content:center;"
+                class="w-full inline-flex shadow items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
+                <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-3 justify-center items-center" style="background-color:#ec4899;">Ups!</span>
+                <span class="inline-flex px-2" style="color:#ec4899;">{{ $message }} </span>
+            </div>
+        </div>
+    @enderror
+{{-- ----------------------------------------------------- --}}
